7e2fb05cd74cd038ab5caf04a068f7f014dbef94
Erik Hollensbe <git@hollensbe.org>
2023-09-21 11:10:35 -0700

    writing more intelligently, and supporting the update_interval
    Signed-off-by: Erik Hollensbe <git@hollensbe.org>

--- src/bar.rs
+++ src/bar.rs
@@ -2,7 +2,7 @@ use crate::{collectors::Collection, config::Config};
 use anyhow::Result;
 use serde_derive::{Deserialize, Serialize};
 use std::collections::BTreeMap;
-use tokio::sync::mpsc::UnboundedReceiver;
+use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver};
 
 #[derive(Debug, Clone, Default)]
 pub struct Bar {
@@ -10,10 +10,24 @@ pub struct Bar {
 }
 
 impl Bar {
+    pub async fn write_blocks(
+        &self,
+        mut w: impl std::io::Write + Send + 'static,
+        mut data: UnboundedReceiver<Vec<Block>>,
+    ) -> Result<()> {
+        while let Some(v) = data.recv().await {
+            serde_json::to_writer(&mut w, &v)?;
+            w.write_all(",\n".as_bytes())?;
+            w.flush()?;
+        }
+
+        Ok(())
+    }
+
     pub async fn emit_status(
         &mut self,
         config: Config,
-        mut w: impl std::io::Write,
+        mut w: impl std::io::Write + Send + 'static,
         mut data: UnboundedReceiver<Collection>,
     ) -> Result<()> {
         serde_json::to_writer(
@@ -26,20 +40,28 @@ impl Bar {
         w.write_all("\n[\n".as_bytes())?;
         w.flush()?;
 
+        let (s, r) = unbounded_channel();
+        let obj = self.clone();
+        tokio::spawn(async move { obj.write_blocks(w, r).await.unwrap() });
+
+        let mut last_send = chrono::Local::now() - config.update_interval();
+
         while let Some(collection) = data.recv().await {
             let block = collection.to_block();
             self.state.insert(collection.name(), block);
-            let mut v = Vec::new();
-            let items = &config.pages()[0].items();
-            for item in items {
-                if let Some(block) = self.state.get(&item.name) {
-                    v.push(block)
-                }
-            }
 
-            serde_json::to_writer(&mut w, &v)?;
-            w.write_all(",\n".as_bytes())?;
-            w.flush()?;
+            let now = chrono::Local::now();
+            if last_send + config.update_interval() < now {
+                let mut v = Vec::new();
+                let items = &config.pages()[0].items();
+                for item in items {
+                    if let Some(block) = self.state.get(&item.name) {
+                        v.push(block.clone())
+                    }
+                }
+                s.send(v)?;
+                last_send = now;
+            }
         }
 
         Ok(())
